703794e3aa75583ce8d1d299ada7d6cb109d5108
polizoto <polizoto@hotmail.com>
2022-02-15 16:53:41 -0500

    Added Helper functions via Handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,7 +8,14 @@ module.exports = {
         if (amount !== 1) {
           return `${word}s`;
         }
-    
         return word;
+      },
+      format_url: url => {
+        return url
+          .replace('http://', '')
+          .replace('https://', '')
+          .replace('www.', '')
+          .split('/')[0]
+          .split('?')[0];
       }
   }
